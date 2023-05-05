d238f4717862952cf6031d74ae7a22163e714793
JonathanLeal <jonathan.e.leal.m@gmail.com>
2023-05-05 09:07:19 -0600

    Desarrollando endpoints para get

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Http;
+use App\Models\Persona;
 use Illuminate\Http\Request;
 
 class PersonaController extends Controller
 {
-    //
+    public function obtenerPersonas(){
+        $personas = Persona::all();
+        if (count($personas) == 0) {
+            return Http::respuesta(http::retNotFound, "no hay personas");
+        }
+        return http::respuesta(http::retOK, $personas);
+    }
+
+    public function obtenerPersonaPorId(Request $request){
+        $idPersona = Persona::find($request->id);
+        if (!$idPersona) {
+            return http::respuesta(http::retNotFound, "no se encontro ese ID");
+        }
+        return http::respuesta(http::retOK, $idPersona);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/personas/list', [PersonaController::class, 'obtenerPersonas']);
+Route::get('/personas/{id}', [PersonaController::class, 'obtenerPersonaPorId']);
